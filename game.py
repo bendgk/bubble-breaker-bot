71dd53a5f807093dafa5907b79d6697cdb99c8f5
bendgk <benkosten@gmail.com>
2018-07-26 19:27:01 -0400

    Add colors and organize the render into hexagons

--- game.py
+++ game.py
@@ -2,9 +2,19 @@ import numpy as np
 import pprint
 from enum import Enum
 import random
+from colorama import init
+from colorama import Fore, Back, Style
 
-from timeit import default_timer as timer
-
+case = {
+    -1: Fore.BLACK + Back.RED + "- ",
+    0:  "  ",
+    1:  Fore.RED + Back.BLACK + "R ",
+    2:  Fore.YELLOW + Back.BLACK + "O ",
+    3:  Fore.GREEN + Back.BLACK + "G ",
+    4:  Fore.MAGENTA + Back.BLACK + "P ",
+    5:  Fore.CYAN + Back.BLACK + "b ",
+    6:  Fore.BLUE + Back.BLACK + "B "
+}
 
 class Game:
     def __init__(self, width: int, height: int):
@@ -14,31 +24,29 @@ class Game:
         self.map = np.zeros(shape=(width, height), dtype=int)
 
         #populate map
-        random.seed(a=None)
+        random.seed(a=1)
         self.populate()
 
     def boundtest(func):
-        start = timer()
-        print(dir(func))
-        print(func.__globals__)
         def onDecorator(self, x, y, *args, **kwargs):
             assert x in range(game.width) and y in range(game.height)
             return func(self, x, y, *args, **kwargs)
 
-        print(timer() - start)
         return onDecorator
 
     def populate(self):
         for y in range(8):
             for x in range(self.width - 1):
                 self.map[x + y%2, y] = random.randint(1, 6)
+
+            self.map[(self.width - 1) * ((y+1)%2), y] = -1
     
-    #@boundtest
+    @boundtest
     def at(self, x: int, y: int) -> int:
         return self.map[x, y]
     
 
-    #@boundtest
+    @boundtest
     def set(self, x: int, y: int, data):
         self.map[x, y] = data.value
         
@@ -46,14 +54,15 @@ class Game:
         self.map = np.zeros(shape=(self.width, self.height), dtype=int)
             
     def render(self):
-        b = ""
+        out = ""
         for y in range(self.height):
+            b = ""
             for x in range(self.width):
-                if self.map[x, y] == 0: b += "  "
-                else: b += str(self.map[x, y]) + " "
-            b += "\n"
+                b += case[self.map[x, y]] + Fore.RESET + Back.RESET
+                
+            out += "-"*((y + 1)%2) + b + "-"*(y%2) + "\n"
 
-        print(b)
+        print(out)
 
 class Color(Enum):
     RED         = 1
@@ -64,6 +73,7 @@ class Color(Enum):
     DARK_BLUE   = 6
 
 if __name__ == '__main__':
-    game = Game(12, 17)
+    init()
+    game = Game(12, 18)
     game.set(1, 1, Color.RED)
     game.render()

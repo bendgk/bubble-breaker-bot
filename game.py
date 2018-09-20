6e7b028c2dfca8613039b6ea91493d7b33d78ce1
bendgk <benkosten@gmail.com>
2018-09-20 18:10:49 -0400

    add a lot of progress ...

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import numpy as np
-import pprint
 from enum import Enum
-import random
-from colorama import init
-from colorama import Fore, Back, Style
+from colorama import init, Fore, Back, Style
+from collections import namedtuple
+from line_utils import evenr_linedraw
+import math, random
+
 
 case = {
     -1: Fore.BLACK + Back.RED + "XX",
@@ -13,7 +14,8 @@ case = {
     3:  Fore.GREEN + Back.BLACK + "G ",
     4:  Fore.MAGENTA + Back.BLACK + "P ",
     5:  Fore.CYAN + Back.BLACK + "b ",
-    6:  Fore.BLUE + Back.BLACK + "B "
+    6:  Fore.BLUE + Back.BLACK + "B ",
+    -2:  Fore.WHITE + Back.RED + "+ "
 }
 
 case_raw = {
@@ -24,7 +26,8 @@ case_raw = {
     3:  Fore.GREEN + Back.BLACK + "3",
     4:  Fore.MAGENTA + Back.BLACK + "4",
     5:  Fore.CYAN + Back.BLACK + "5",
-    6:  Fore.BLUE + Back.BLACK + "6"
+    6:  Fore.BLUE + Back.BLACK + "6",
+    -2:  Fore.WHITE + Back.RED + "+"
 }
 
 class Color(Enum):
@@ -34,6 +37,7 @@ class Color(Enum):
     PURPLE      = 4
     LIGHT_BLUE  = 5
     DARK_BLUE   = 6
+    DEBUG       = -2
 
 
 class RenderMode(Enum):
@@ -41,41 +45,50 @@ class RenderMode(Enum):
     HEX = 0
     GRID = 1
     RAW = 2
+
+Point = namedtuple('Point', ['x', 'y'])
     
 class Game:
     def __init__(self, width: int, height: int):
-        self.map = np.zeros(shape=(width, height), dtype=int)
-        self.width = width - 1
-        self.height = height - 1
+        self.width = width + 1
+        self.height = height
         
+        self.origin = Point(int(self.width/2), 0)
+
+        self.bubbles = []
 
         #populate map
-        random.seed(a=1)
-        self.populate()
+        self.reset_game()
 
     def boundtest(func):
         def onDecorator(self, x, y, *args, **kwargs):
-            assert x in range(game.width) and y in range(game.height)
+            assert x in range(game.width), f"Out of bounds! x: {x} <--, y: {y}"
+            assert y in range(game.height + 1), f"Out of bounds! x: {x}, y: {y} <--"
             return func(self, x, y, *args, **kwargs)
 
         return onDecorator
 
-    def populate(self):
+    def reset_game(self):
+        random.seed(a=1337)
+        self.map = np.zeros(shape=(self.width + 1, self.height + 1), dtype=int)
+        
         for y in range(self.height):
             for x in range(self.width):
                 if y < 8:
-                    self.map[x + y%2, y] = random.randint(1, 6)
+                    color = random.randint(1, 6)
+                    self.map[x + y%2, y] = color
+                    self.bubbles.append(Point(x + y%2, y))
 
             self.map[(self.width - 1) * ((y+1)%2), y] = -1
     
     @boundtest
     def at(self, x: int, y: int) -> int:
-        return self.map[x, y]
+        return self.map[x, self.height - y - 1]
     
 
     @boundtest
     def set(self, x: int, y: int, data):
-        self.map[x, y] = data.value
+        self.map[x, self.height - y - 1] = data.value
             
     def render(self, mode=RenderMode.DEFAULT):
         out = ""
@@ -92,8 +105,56 @@ class Game:
 
         print(out)
 
+class TrainingEnvironment:
+    def __init__(self):
+        self.game = Game(11, 17)
+
+    def get_action_space(self):
+        pass
+    def get_observable_space(self):
+        pass
+
 if __name__ == '__main__':
     init()
     game = Game(11, 17)
-    game.set(1, 1, Color.LIGHT_BLUE)
-    game.render(mode = RenderMode.RAW)
+
+    game.render()
+
+    results = set()
+    
+    for y in range(2, game.height):
+        collided = False
+
+        start = game.origin
+        end = Point(0, y)
+
+        dy = (end.y - start.y) * 2
+        dx = (end.x - start.x) * 2
+        
+        while not collided:
+            line = evenr_linedraw(start, end)
+
+            for i in range(1, len(line)):
+                if game.at(line[i].x, line[i].y) > 0:
+                    collided = True
+                    print("Collided at:", line[i - 1])
+                    results.add(line[i - 1])
+                    break
+
+                game.set(line[i].x, line[i].y, Color.DEBUG)
+
+                
+            ##########################################################
+            #                       wall logic                       #
+            ##########################################################
+            start = end
+            dx = -dx
+            end = Point(start.x + int(np.sign(dx)*11), start.y + abs(int(dy/dx*11)))
+
+            game.render()
+            print(start, end)
+            input()
+
+        game.reset_game()
+
+    print(len(results))
---
+++ line_utils.py
@@ -0,0 +1,111 @@
+import numpy as np
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+Cube = namedtuple('Cube', ['x', 'y', 'z'])
+
+dirs = lambda i, j, current: {
+    (-1, -1): Point(current.x - current.y%2, current.y - 1), #DOWN_LEFT
+    (-1, 1): Point(current.x - current.y%2, current.y + 1), #UP_LEFT
+    (0, -1): Point(current.x - 1, current.y), #LEFT
+    (0, 1): Point(current.x + 1, current.y), #RIGHT
+    (1, -1): Point(current.x + (current.y + 1)%2, current.y - 1), #DOWN_RIGHT
+    (1, 1): Point(current.x + (current.y + 1)%2, current.y + 1), #UP_RIGHT
+}[(i, j)]
+
+def line_of_sight(start: Point, end: Point):
+    current = start
+
+    dx = end.x - start.x
+    dy = end.y - start.y
+
+    x_sign = np.sign(dx)
+    y_sign = np.sign(dy)
+
+    dx = abs(dx)
+    dy = abs(dy)
+    
+    e = 0
+
+    points = []
+    
+    while current != end:
+        if e > 1:
+            current = dirs(-x_sign, y_sign, current) #UP_LEFT
+            e = e - (3*dy)/(4*dx) - 1.5
+
+        else:
+            e = e + (3*dy)/(4*dx)
+
+            if e > 0.5:
+                current = dirs(x_sign, y_sign, current) #UP_RIGHT
+                e = e - 1.5
+
+            else:
+                if e < -0.5:
+                    current = dirs(x_sign, -y_sign, current) #DOWN_RIGHT
+                    e = e + 1.5
+
+                else:
+                    current = dirs(0, x_sign, current) #RIGHT
+                    e = e = e + (3*dy)/(4*dx)
+  
+        points.append(current)
+
+    return points
+
+def evenr_to_cube(point: Point):
+    x = point.x - (point.y + (point.y&1))/2
+    z = point.y
+    y = -x - z
+
+    return Cube(x, y, z)
+
+def cube_to_evenr(cube: Cube):
+    x = int(cube.x + (cube.z + (cube.z&1)) / 2)
+    y = int(cube.z)
+
+    return Point(x, y)
+
+def lerp(a, b, t):
+    return a + (b - a) * t
+
+def cube_lerp(a, b, t):
+    return Cube(lerp(a.x, b.x, t), 
+                lerp(a.y, b.y, t),
+                lerp(a.z, b.z, t))
+
+def cube_distance(a, b):
+    return int(max(abs(a.x - b.x), abs(a.y - b.y), abs(a.z - b.z)))
+
+def cube_round(cube: Cube):
+    rx = round(cube.x)
+    ry = round(cube.y)
+    rz = round(cube.z)
+
+    x_diff = abs(rx - cube.x)
+    y_diff = abs(ry - cube.y)
+    z_diff = abs(rz - cube.z)
+
+    if x_diff > y_diff and x_diff > z_diff:
+        rx = -ry-rz
+    elif y_diff > z_diff:
+        ry = -rx-rz
+    else:
+        rz = -rx-ry
+
+    return Cube(rx, ry, rz)
+
+def evenr_linedraw(a: Point, b: Point):
+    a = evenr_to_cube(a)
+    b = evenr_to_cube(b)
+    
+    N = cube_distance(a, b)
+    results = []
+    
+    for i in range(N + 1):
+        results.append(cube_to_evenr(cube_round(cube_lerp(a, b, 1.0/N * i))))
+
+    return results
+
+print(evenr_linedraw(Point(6, 0), Point(0, 5)))
